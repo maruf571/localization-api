5a4b984b07e84d96f5655256a9830c34df47e718
Maruf <mdmahmudul.hassan@un.org>
2018-10-16 17:51:17 +0700

    remove old ui code

---
+++ localization-api/src/main/resources/liquibase/liquibase-changeLog.mysql.sql
@@ -0,0 +1,3 @@
+--liquibase formatted sql
+
+
--- localization-ui/src/environments/environment.prod.ts
+++ localization-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  REST_API_URL: 'http://localhost:7171/api/'
+  REST_API_URL: 'http://localization.marufh.com:7070/api/'
 };
