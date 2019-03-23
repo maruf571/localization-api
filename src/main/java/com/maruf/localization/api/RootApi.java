e2e68aa6a8965f6fe03c9c86c3c13270ec82bc7c
maruf <agile.out@gmail.com>
2019-03-23 20:56:45 +0100

    update

--- src/main/java/com/maruf/localization/api/RootApi.java
+++ src/main/java/com/maruf/localization/api/RootApi.java
@@ -10,6 +10,6 @@ public class RootApi {
 
     @GetMapping
     public String welcomeMessage(){
-        return "Welcome to Localization with 02";
+        return "Welcome to Localization with 03";
     }
 }
